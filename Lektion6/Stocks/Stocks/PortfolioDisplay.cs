36c7752ca3f45f841d0b62f22034f61bd22c6c28
Tjsaaby <Tjsaaby@outlook.dk>
2020-10-09 16:08:48 +0200

    PortfolioDisply - adding print function and creating new event in IPortfolio

--- Lektion6/Stocks/Stocks/PortfolioDisplay.cs
+++ Lektion6/Stocks/Stocks/PortfolioDisplay.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Stocks.Interfaces;
 
 namespace Stocks
 {
-    public class PortfolioDisplay
+    public class PortfolioDisplay : IPortfolioDisplay
     {
+        private readonly IPortfolio _portfolio;
+        public PortfolioDisplay(IPortfolio portfolio)
+        {
+            _portfolio = portfolio;
+            _portfolio.PortfolioPricesUpdated += OnPortfolioPricesUpdated;
+        }
+
+        private void OnPortfolioPricesUpdated(object? sender, EventArgs e)
+        {
+            DisplayPortfolio();
+        }
+
+        private void printer()
+        {
+            Console.WriteLine("Symbol \t Name \t Number of stocks \t Price per stock \t Total stock value");
+            foreach (var s in _portfolio.GetPortfolioStockInformations()) //insert dummy portfolio
+            {
+                Console.WriteLine("{2} \t \t \t \t \t \t \t {2} \t {2} \t {2} ", s.StockSymbol, s.StockName, s.NumberOfStocks, s.StockValue);
+            }
+        }
+
+
+        public void DisplayPortfolio()
+        {
+            printer();
+        }
     }
 }
--- Lektion6/Stocks/Stocks/Stock.cs
+++ Lektion6/Stocks/Stocks/Stock.cs
@@ -1,13 +1,14 @@
 ﻿using Stocks.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Stocks
 {
     public class Stock : IStock
     {
-
         public Stock(string name, string symbol)
         {
             Name = name;
