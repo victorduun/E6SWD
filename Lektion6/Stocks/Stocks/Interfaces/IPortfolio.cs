610f0e54778d9b6dbfda3073794da9464a384a59
Tjsaaby <Tjsaaby@outlook.dk>
2020-10-09 16:11:38 +0200

    adding IPortfolio file and journal

---
+++ Lektion6/Stocks/Stocks/Interfaces/IPortfolio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stocks.Interfaces
+{
+    public class StockInformationDto
+    {
+        public string StockName { get; set; }
+        public string StockSymbol { get; set; }
+        public int NumberOfStocks { get; set; }
+        public int StockValue { get; set; }
+    }
+
+
+    public interface IPortfolio
+    {
+        event EventHandler PortfolioPricesUpdated; 
+        void AddStock(IStock stock);
+        IEnumerable<StockInformationDto> GetPortfolioStockInformations();
+        double GetPortfolioValue();
+    }
+}
